927d9d18f365f90a33121b25b0d1abf0982e0e44
zhiren2202 <132483183+zhiren2202@users.noreply.github.com>
2023-07-12 15:13:13 +0900

    Add files via upload

--- resources/js/data.js
+++ resources/js/data.js
@@ -13,146 +13,152 @@ $.datepicker.setDefaults({
 });
 
 $(function () {
-    $("#applyDate0").datepicker({
-      changeMonth: true,
-      changeYear: true,
-    });
-    $('#applyDate0').datepicker('setDate', 'today');
+  $("#birthDate").datepicker({
+    changeMonth: true,
+    changeYear: true,
   });
+});
 
-  $(function () {
-    $("#applyDate1").datepicker({
-      changeMonth: true,
-      changeYear: true,
-    });
-    $('#applyDate1').datepicker('setDate', 'today');
+$(function () {
+  $("#applyDate0").datepicker({
+    changeMonth: true,
+    changeYear: true,
   });
+  $("#applyDate0").datepicker("setDate", "today");
+});
 
-  $(function () {
-    $("#applyDate2").datepicker({
-      changeMonth: true,
-      changeYear: true,
-    });
-    $('#applyDate2').datepicker('setDate', 'today');
+$(function () {
+  $("#applyDate1").datepicker({
+    changeMonth: true,
+    changeYear: true,
   });
+  $("#applyDate1").datepicker("setDate", "today");
+});
 
-  $(function () {
-      $("#applyDate3").datepicker({
-        changeMonth: true,
-        changeYear: true,
-      });
-      $('#applyDate3').datepicker('setDate', 'today');
-    });
-  
-    $(function () {
-      $("#applyDate4").datepicker({
-        changeMonth: true,
-        changeYear: true,
-      });
-      $('#applyDate4').datepicker('setDate', 'today');
-    });
-  
-    $(function () {
-      $("#applyDate5").datepicker({
-        changeMonth: true,
-        changeYear: true,
-      });
-      $('#applyDate5').datepicker('setDate', 'today');
-    });
-
-  $(function () {
-    $("#applyDate2_0").datepicker({
-      changeMonth: true,
-      changeYear: true,
-    });
-    $('#applyDate2_0').datepicker('setDate', 'today');
+$(function () {
+  $("#applyDate2").datepicker({
+    changeMonth: true,
+    changeYear: true,
   });
+  $("#applyDate2").datepicker("setDate", "today");
+});
 
-  $(function () {
-    $("#applyDate2_1").datepicker({
-      changeMonth: true,
-      changeYear: true,
-    });
-    $('#applyDate2_1').datepicker('setDate', 'today');
+$(function () {
+  $("#applyDate3").datepicker({
+    changeMonth: true,
+    changeYear: true,
   });
+  $("#applyDate3").datepicker("setDate", "today");
+});
 
-  $(function () {
-    $("#applyDate2_2").datepicker({
-      changeMonth: true,
-      changeYear: true,
-    });
-    $('#applyDate2_2').datepicker('setDate', 'today');
+$(function () {
+  $("#applyDate4").datepicker({
+    changeMonth: true,
+    changeYear: true,
   });
+  $("#applyDate4").datepicker("setDate", "today");
+});
 
-  $(function () {
-    $("#applyDate2_3").datepicker({
-      changeMonth: true,
-      changeYear: true,
-    });
-    $('#applyDate2_3').datepicker('setDate', 'today');
+$(function () {
+  $("#applyDate5").datepicker({
+    changeMonth: true,
+    changeYear: true,
   });
+  $("#applyDate5").datepicker("setDate", "today");
+});
 
-  $(function () {
-    $("#applyDate2_4").datepicker({
-      changeMonth: true,
-      changeYear: true,
-    });
-    $('#applyDate2_4').datepicker('setDate', 'today');
+$(function () {
+  $("#applyDate2_0").datepicker({
+    changeMonth: true,
+    changeYear: true,
   });
+  $("#applyDate2_0").datepicker("setDate", "today");
+});
 
-  $(function () {
-    $("#applyDate2_5").datepicker({
-      changeMonth: true,
-      changeYear: true,
-    });
-    $('#applyDate2_5').datepicker('setDate', 'today');
+$(function () {
+  $("#applyDate2_1").datepicker({
+    changeMonth: true,
+    changeYear: true,
   });
+  $("#applyDate2_1").datepicker("setDate", "today");
+});
 
-  $(function () {
-    $("#applyDate2_6").datepicker({
-      changeMonth: true,
-      changeYear: true,
-    });
-    $('#applyDate2_6').datepicker('setDate', 'today');
+$(function () {
+  $("#applyDate2_2").datepicker({
+    changeMonth: true,
+    changeYear: true,
   });
+  $("#applyDate2_2").datepicker("setDate", "today");
+});
 
-  $(function () {
-    $("#applyDate2_7").datepicker({
-      changeMonth: true,
-      changeYear: true,
-    });
-    $('#applyDate2_7').datepicker('setDate', 'today');
+$(function () {
+  $("#applyDate2_3").datepicker({
+    changeMonth: true,
+    changeYear: true,
   });
+  $("#applyDate2_3").datepicker("setDate", "today");
+});
 
-  $(function () {
-    $("#applyDate2_8").datepicker({
-      changeMonth: true,
-      changeYear: true,
-    });
-    $('#applyDate2_8').datepicker('setDate', 'today');
+$(function () {
+  $("#applyDate2_4").datepicker({
+    changeMonth: true,
+    changeYear: true,
   });
+  $("#applyDate2_4").datepicker("setDate", "today");
+});
 
-  $(function () {
-    $("#applyDate2_9").datepicker({
-      changeMonth: true,
-      changeYear: true,
-    });
-    $('#applyDate2_9').datepicker('setDate', 'today');
+$(function () {
+  $("#applyDate2_5").datepicker({
+    changeMonth: true,
+    changeYear: true,
   });
+  $("#applyDate2_5").datepicker("setDate", "today");
+});
 
-  // 관련 자료 등록
-  $(document).ready(function(){
-    var fileTarget = $('#input-file');
-  
-    fileTarget.on('change', function(){
-      if(window.FileReader){
-        var filename = $(this)[0].files[0].name;
-      } 
-      else { 
-        var filename = $(this).val().split('/').pop().split('\\').pop();
-      }
-      
-      $(this).siblings('.upload-name').val(filename);
-      $(this).siblings('.upload-name').show().stop();
-    });
+$(function () {
+  $("#applyDate2_6").datepicker({
+    changeMonth: true,
+    changeYear: true,
   });
+  $("#applyDate2_6").datepicker("setDate", "today");
+});
+
+$(function () {
+  $("#applyDate2_7").datepicker({
+    changeMonth: true,
+    changeYear: true,
+  });
+  $("#applyDate2_7").datepicker("setDate", "today");
+});
+
+$(function () {
+  $("#applyDate2_8").datepicker({
+    changeMonth: true,
+    changeYear: true,
+  });
+  $("#applyDate2_8").datepicker("setDate", "today");
+});
+
+$(function () {
+  $("#applyDate2_9").datepicker({
+    changeMonth: true,
+    changeYear: true,
+  });
+  $("#applyDate2_9").datepicker("setDate", "today");
+});
+
+// 관련 자료 등록
+$(document).ready(function () {
+  var fileTarget = $("#input-file");
+
+  fileTarget.on("change", function () {
+    if (window.FileReader) {
+      var filename = $(this)[0].files[0].name;
+    } else {
+      var filename = $(this).val().split("/").pop().split("\\").pop();
+    }
+
+    $(this).siblings(".upload-name").val(filename);
+    $(this).siblings(".upload-name").show().stop();
+  });
+});
