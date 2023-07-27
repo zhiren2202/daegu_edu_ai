c74658c5a1f142a6fd98ad218880ab7555b620cd
zhiren2202 <132483183+zhiren2202@users.noreply.github.com>
2023-07-27 17:45:04 +0900

    Add files via upload

--- resources/js/top_button.js
+++ resources/js/top_button.js
@@ -1,3 +1,17 @@
+// $(function () {
+//   $(window).scroll(function () {
+//     if ($(this).scrollTop() > 250) {
+//       $(".top_btn").fadeIn();
+//     } else {
+//       $(".top_btn").fadeOut();
+//     }
+//   });
+//   $(".top_btn").click(function () {
+//     $("html, body").animate({ scrollTop: 0 }, 400); // 속도 400
+//     return false;
+//   });
+// });
+
 $(function () {
   $(window).scroll(function () {
     if ($(this).scrollTop() > 250) {
